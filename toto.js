2f99b30f93944ed7b0b74fb75f0c37a278bd33b0
SamanthaNyota <samanthadray@hotmail.fr>
2017-11-06 13:47:39 +0100

    modif toto

--- toto.js
+++ toto.js
@@ -1,4 +1,8 @@
-hQUESADILLASolCOMPLEANOSa JAVIERe tal?
+Hola que tal?
 
 chocolate for christmas
-
+hoping for snow... 
+for christmas? 
+yeah! 
+I hope i'll be able to play on the snow for christmas.
+what? Play on the snow? I'm not hoping for snow, i'm hoping for Jon Snow! 
